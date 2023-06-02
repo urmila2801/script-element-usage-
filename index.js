a05ecddb9cf1b775e26728c6b1365b2d573ddaf5
urmila2801 <urmilapamarthi3@gmail.com>
2023-06-02 06:50:42 +0000

    scriptElement

---
+++ index.js
@@ -0,0 +1,7 @@
+const element = (
+  <div>
+    <h1 className="greeting"> Hello Divya </h1>
+    <p className="Proposal"> How R U </p>
+  </div>
+);
+ReactDOM.render(element, document.getElementById("root"));
